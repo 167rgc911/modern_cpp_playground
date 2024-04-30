ebc2b127ccadd9cf41b4917fc01370ee024de31d
rgc <74342662+167rgc911@users.noreply.github.com>
2024-04-30 22:27:18 +0000

    update; sanitizing mandatory, running fuzzer

--- readtextfile/bench_readtextfile.cpp
+++ readtextfile/bench_readtextfile.cpp
@@ -28,14 +28,6 @@ test (std::string test_string0)
 {
   int retval = 0;
 
-  // sanitize
-  test_string0.erase (
-      std::remove_if (test_string0.begin (), test_string0.end (),
-                      [] (unsigned char c) { return std::ispunct (c); }),
-      test_string0.end ());
-
-  // input text file -> std::vector
-  // can be n .. n + x lines
   const auto lines_ = read_string_lines (test_string0, retval);
 
   std::vector<std::string> ll;
@@ -74,14 +66,6 @@ test_lambda (std::string test_string0)
 {
   int retval = 0;
 
-  // sanitize
-  test_string0.erase (
-      std::remove_if (test_string0.begin (), test_string0.end (),
-                      [] (unsigned char c) { return std::ispunct (c); }),
-      test_string0.end ());
-
-  // input text file -> std::vector
-  // can be n .. n + x lines
   const auto lines_ = read_string_lines (test_string0, retval);
 
   std::vector<std::string> ll;
@@ -111,45 +95,5 @@ BM_test_lambda (benchmark::State &state)
 }
 
 BENCHMARK (BM_test_lambda);
-
-int
-test_wos (std::string test_string0)
-{
-  int retval = 0;
-
-  // input text file -> std::vector
-  // can be n .. n + x lines
-  const auto lines_ = read_string_lines (test_string0, retval);
-
-  std::vector<std::string> ll;
-  for (const auto &line : lines_)
-    {
-      auto r_ = split (line, ' ', retval);
-      auto me = std::max_element (r_.begin (), r_.end (),
-                                  [] (std::string &s1, std::string &s2) {
-                                    return s1.length () < s2.length ();
-                                  });
-      if (me != r_.end ())
-        {
-          ll.push_back (*me);
-        }
-    }
-
-  auto m = std::max_element (ll.begin (), ll.end (),
-                             [] (std::string &s1, std::string &s2) {
-                               return s1.length () < s2.length ();
-                             });
-  return 0;
-}
-
-static void
-BM_test_wos (benchmark::State &state)
-{
-  std::string test_string0 = lorem_ipsum::generate_lorem_ipsum_lines (24);
-  for (auto _ : state)
-    test_wos (test_string0);
-}
-
-BENCHMARK (BM_test_wos);
 
 BENCHMARK_MAIN ();
