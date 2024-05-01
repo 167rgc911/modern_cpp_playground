648b528c75debbe65408b06edd5cbd38c416e25f
rgc <74342662+167rgc911@users.noreply.github.com>
2024-05-01 05:43:55 +0000

    update; fuzzer, is this correct?

---
+++ readtextfile/fuzz_readtextfile.cpp
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fuzz_readtextfile.cpp
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  05/01/24 06:05:37
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rgc (),
+ *   Organization:
+ *
+ * SPDX-License-Identifier: MIT
+ * =====================================================================================
+ */
+
+#include <algorithm>
+#include <fstream>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "readtextfile.h"
+
+extern "C" int
+LLVMFuzzerTestOneInput (const uint8_t *Data, size_t Size)
+{
+  int retval = 0;
+  std::string s(reinterpret_cast<const char*>(Data), Size);
+  std::optional<std::vector<std::string>> discarded = read_string_lines (s, retval);
+  return retval; // Values other than 0 and -1 are reserved for future use.
+}
+
