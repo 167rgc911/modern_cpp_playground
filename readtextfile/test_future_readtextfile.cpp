1d7089a720d80f9fc3cd041a6b46806695d40822
rgc <74342662+167rgc911@users.noreply.github.com>
2024-05-02 21:29:10 +0000

    update; futures, i really see no point since the process is very sequential

--- readtextfile/test_future_readtextfile.cpp
+++ readtextfile/test_future_readtextfile.cpp
@@ -28,17 +28,23 @@
 
 #include "readtextfile.h"
 
+std::vector<std::string>
+get_lines(int &retval)
+{
+  retval = 0;
+  // generate text; note that not all 24 lines/rows end with '\n'
+  auto test_string0 = lorem_ipsum::generate_lorem_ipsum_lines (24);
+  return read_string_lines (test_string0, retval);
+}
+
 int
 main (void)
 {
   int retval = 0;
 
-  // generate text
-  std::string test_string0 = lorem_ipsum::generate_lorem_ipsum_lines (24);
-
-  std::future<std::vector<std::string> > lines_
+  std::future<std::vector<std::string>> lines_
       = std::async (std::launch::async, [&] () {
-          return read_string_lines (test_string0, retval);
+          return get_lines (retval);
         });
 
   std::vector<std::string> ll;
@@ -52,13 +58,15 @@ main (void)
       if (me != r_.end ())
         {
           ll.push_back (*me);
-          // longest word for each input line
-          std::cout << *me << '\n';
         }
     }
 
+  // sort and remove dupes
+  std::sort (ll.begin (), ll.end ());
+  ll.erase (std::unique (ll.begin (), ll.end ()), ll.end ());
+
   auto m = std::max_element (ll.begin (), ll.end (), f);
-  // does not account for other words
+  // does not account for other words with the same max length
   if (m != ll.end ())
     {
       std::cout << "longest word: " << *m << '\n';
