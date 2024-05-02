345cb620a2829e7f0fda570578e324df9e0c4c9e
rgc <74342662+167rgc911@users.noreply.github.com>
2024-05-02 11:02:03 +0000

    normalize the string to all lowercase

--- src/readtextfile.h
+++ src/readtextfile.h
@@ -63,6 +63,10 @@ read_string_lines (std::string &s, int &retval)
           return {};
         }
 
+      // normalize to all ASCII lowercase
+      std::transform (s.begin (), s.end (), s.begin (),
+          [](unsigned char c) { return std::tolower(c); });
+
       return split (s, '\n', retval);
     }
 
