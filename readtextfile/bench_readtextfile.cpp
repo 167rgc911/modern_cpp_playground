ca3316220d9c889df6473678f73c0fc0db8d37fe
rgc <74342662+167rgc911@users.noreply.github.com>
2024-05-03 09:22:43 +0000

    ranges2 bm; penalty

--- readtextfile/bench_readtextfile.cpp
+++ readtextfile/bench_readtextfile.cpp
@@ -159,4 +159,56 @@ BM_test_ranges (benchmark::State &state)
 
 BENCHMARK (BM_test_ranges);
 
+int
+test_ranges2 (std::string test_string0)
+{
+  int retval = 0;
+
+  auto lines_ = read_string_lines (test_string0, retval);
+
+  if (not lines_.empty ())
+    {
+      std::vector<std::string> ll{};
+
+      auto f = [] (std::string &s1, std::string &s2) {
+        return s1.length () < s2.length ();
+      };
+      for (auto &line : lines_)
+        {
+          auto r_ = split (line, ' ', retval);
+          auto me = std::max_element (r_.begin (), r_.end (), f);
+          if (me != r_.end ())
+            {
+              ll.push_back (*me);
+            }
+        }
+
+      // remove dupes
+      auto [srf, srl] = std::ranges::unique (ll.begin (), ll.end ());
+      ll.erase (srf, srl);
+
+      // get the longest element
+      auto m = std::max_element (ll.begin (), ll.end (), f);
+      if (m != ll.end ())
+        {
+          auto lw_sz = m->length ();
+          // get a view that contains strings with max length
+          auto lw_vw = std::views::filter (
+              ll, [lw_sz] (std::string s) { return s.length () == lw_sz; });
+        }
+    }
+
+  return retval;
+}
+
+static void
+BM_test_ranges2 (benchmark::State &state)
+{
+  std::string test_string0 = lorem_ipsum::generate_lorem_ipsum_lines (24);
+  for (auto _ : state)
+    test_ranges2 (test_string0);
+}
+
+BENCHMARK (BM_test_ranges2);
+
 BENCHMARK_MAIN ();
