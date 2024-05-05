44aeb7c5549450a03a84169143ed99bfa2492d37
rgc <74342662+167rgc911@users.noreply.github.com>
2024-05-05 11:32:14 +0000

    update; removing dupes using ranges

--- readtextfile/test_fp_readtextfile.cpp
+++ readtextfile/test_fp_readtextfile.cpp
@@ -63,7 +63,8 @@ main (void)
         }
 
       // remove dupes
-      ll.erase (std::unique (ll.begin (), ll.end ()), ll.end ());
+      auto [srf, srl] = std::ranges::unique (ll.begin (), ll.end ());
+      ll.erase (srf, srl);
 
       // get the longest element
       auto m = std::max_element (ll.begin (), ll.end (), f);
